eb0b8f969a01b67d2aafc9da484a8a692aada000
Melo <lucabaudo9412@gmail.com>
2026-01-08 16:53:51 +0100

    Optimized contacts and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import '../assets/styles/footer.css'
+import { useIsMobile } from '../isMobile'
+
 function Footer() {
+    const isMobile = useIsMobile(500)
 
     return (<footer className="footer-section">
         <hr className="footer-divider" />
@@ -9,7 +12,7 @@ function Footer() {
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center'
-            }}>© 2024 Luca Baudo. All rights reserved.<br />
+            }}>© 2024 Luca Baudo.{isMobile ? <br /> : ''} All rights reserved.<br />
                 <i>Built with React, Bootstrap & ❤️</i></small>
             <a
                 href="https://github.com/Thelink9412/personal-portfolio-front-end"
