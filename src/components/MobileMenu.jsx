de94cd9d65842e0f8b929b1a6b02f176bd4149a1
Melo <lucabaudo9412@gmail.com>
2026-01-09 11:11:35 +0100

    Added blur effect on mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,18 +1,32 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import mobileMenuIcon from '../assets/images/mobile-menu-icon.png'
 
 function MobileMenu() {
     const [isActive, setIsActive] = useState(false)
 
-    return (<section className={`mobile-menu ${isActive ? 'active' : ''}`}>
-        <img src={mobileMenuIcon} alt="Mobile Menu Icon" onClick={() => setIsActive(prev => !prev)}/>
-        <ul className='links'>
-            <li><a href='#home'>Home</a></li>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#contacts'>Contacts</a></li>
-        </ul>
-    </section>)
+    useEffect(() => {
+        if (isActive) {
+            document.documentElement.classList.add('no-scroll');
+            document.body.classList.add('no-scroll');
+        } else {
+            document.documentElement.classList.remove('no-scroll');
+            document.body.classList.remove('no-scroll');
+        }
+    }, [isActive]);
+
+    return (
+        <>
+            <div className={`blur ${isActive ? 'is-active' : ''}`} onClick={() => setIsActive(prev => !prev)}></div>
+            <section className={`mobile-menu ${isActive ? 'active' : ''}`}>
+                <img src={mobileMenuIcon} alt="Mobile Menu Icon" onClick={() => setIsActive(prev => !prev)} />
+                <ul className='links'>
+                    <li><a href='#home' onClick={() => setIsActive(prev => !prev)}>Home</a></li>
+                    <li><a href='#about' onClick={() => setIsActive(prev => !prev)}>About</a></li>
+                    <li><a href='#projects' onClick={() => setIsActive(prev => !prev)}>Projects</a></li>
+                    <li><a href='#contacts' onClick={() => setIsActive(prev => !prev)}>Contacts</a></li>
+                </ul>
+            </section>
+        </>)
 }
 
 export default MobileMenu
